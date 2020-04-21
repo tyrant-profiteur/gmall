b83c576173d77ba14fece21ae8690b371d4e7e19
szxc_tyrant <674389994@qq.com>
2020-04-21 15:02:43 +0800

    Distributed architecture setup completed

--- gmall-user/src/test/java/com/tyrant/gmall/user/GmallUserApplicationTests.java
+++ gmall-user/src/test/java/com/tyrant/gmall/user/GmallUserApplicationTests.java
@@ -1,13 +1,17 @@
 package com.tyrant.gmall.user;
 
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GmallUserApplicationTests {
+public class GmallUserApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
